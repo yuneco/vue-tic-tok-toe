8bdef1feb6a6b4aa415acdbeedd5e7dde02e2de0
yuneco <yukiyuneco@gmail.com>
2019-12-15 16:25:43 +0900

    Squashed 'tttjs/' content from commit 82ee206
    git-subtree-dir: tttjs
    git-subtree-split: 82ee2065883cf3c5273e6b4e571aa7f8bca9f623

---
+++ src/components/GameCell.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="game-cell"
+    @click="$emit('click')"
+    :style="{
+      backgroundImage: owner ? `url('/imgs/stone_${owner}.svg')` : 'none'
+    }"
+  >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    owner: { type: String }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.game-cell {
+  border: 1px solid gray;
+  background-image: url();
+}
+</style>
