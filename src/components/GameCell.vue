f296ca5070e2c4ba17780fefcba483ca3cec54d1
yuneco <yukiyuneco@gmail.com>
2019-12-15 16:26:18 +0900

    Squashed 'ttt-ts/' content from commit 5b93135
    git-subtree-dir: ttt-ts
    git-subtree-split: 5b931350775ef33b573e448f3a47baaa7302f9e6

---
+++ src/components/GameCell.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="game-cell"
+    @click="$emit('click')"
+    :style="{
+      backgroundImage: owner ? `url('/imgs/stone_${owner}.svg')` : 'none'
+    }"
+  >
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+export default createComponent({
+  props: {
+    owner: { type: String }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.game-cell {
+  border: 1px solid gray;
+}
+</style>
